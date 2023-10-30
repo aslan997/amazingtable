74b1d9629abe1e8055562d276cbf33c992cf6b43
aslan997 <47775067+aslan997@users.noreply.github.com>
2023-10-30 17:26:06 +0500

    Hello

--- src/App.js
+++ src/App.js
@@ -1,148 +1,194 @@
-import React, { useState } from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import { ResizableBox } from 'react-resizable';
-import 'react-resizable/css/styles.css';
-
-const initialColumns = [
-  { id: 'col1', name: 'Name' },
-  { id: 'col2', name: 'Address' },
-  { id: 'col3', name: 'Age' },
-  { id: 'col4', name: 'Gender' },
-  { id: 'col5', name: 'Category' },
-];
-
-const generateRandomData = (column) => {
-  const data = [];
-  for (let i = 0; i < 5; i++) {
-    if (column === 'Name') {
-      data.push(`Name ${i + 1}`);
-    } else if (column === 'Address') {
-      data.push(`Address ${i + 1}`);
-    } else if (column === 'Age') {
-      data.push(Math.floor(Math.random() * 100));
-    } else if (column === 'Gender') {
-      data.push(i % 2 === 0 ? 'Male' : 'Female');
-    } else if (column === 'Category') {
-      data.push(`Category ${i + 1}`);
-    }
-  }
-  return data;
-};
+import React, { Fragment } from "react";
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import "react-resizable/css/styles.css";
 
 const Table = () => {
-  const [columns, setColumns] = useState(initialColumns);
-  const [data, setData] = useState([]);
-  for (let i = 0; i < 5; i++) {
-    data.push({
-      id: `row${i + 1}`,  // Use string IDs like "row1", "row2", etc.
-      ...generateRandomData('Name'),
-    });
+  const user = {
+    name: 'Tom Cook',
+    email: 'tom@example.com',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
   }
+  const navigation = [
+    { name: 'Dashboard', href: '#', current: true },
+    { name: 'Team', href: '#', current: false },
+    { name: 'Projects', href: '#', current: false },
+    { name: 'Calendar', href: '#', current: false },
+    { name: 'Reports', href: '#', current: false },
+  ]
+  const userNavigation = [
+    { name: 'Your Profile', href: '#' },
+    { name: 'Settings', href: '#' },
+    { name: 'Sign out', href: '#' },
+  ]
 
-  const handleDragEnd = (result) => {
-    if (!result.destination) return;
-
-    if (result.type === 'COLUMN') {
-      const newColumns = Array.from(columns);
-      const [removed] = newColumns.splice(result.source.index, 1);
-      newColumns.splice(result.destination.index, 0, removed);
-      setColumns(newColumns);
-    }
-
-    if (result.type === 'ROW') {
-      const newData = [...data];
-      const [moved] = newData.splice(result.source.index, 1);
-      newData.splice(result.destination.index, 0, moved);
-      setData(newData);
-    }
-  };
-
-  const addColumn = () => {
-    const newColumn = prompt('Enter column name');
-    if (newColumn) {
-      setColumns([...columns, { id: `col${columns.length + 1}`, name: newColumn }]);
-    }
-  };
-
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+  
   return (
-    <div className="table-container">
-      <button onClick={addColumn}>Add Column</button>
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <Droppable droppableId="columns" type="COLUMN" direction="horizontal">
-          {(provided) => (
-            <div
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-              className="table"
-            >
-              {columns.map((col, columnIndex) => (
-                <Draggable key={col.id} draggableId={col.id} index={columnIndex} type="COLUMN">
-                  {(provided) => (
-                    <div
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      ref={provided.innerRef}
-                      className="column"
-                    >
-                      <div className="column-header">
-                        <ResizableBox
-                          width={200}
-                          height={30}
-                          minConstraints={[100, Infinity]}
-                          maxConstraints={[400, Infinity]}
-                          axis="x"
-                          handle={<span className="resize-handle" />}
-                        >
-                          {col.name}
-                        </ResizableBox>
-                      </div>
+    <div className="min-h-full">
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 items-center justify-between">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-8 w-8"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company"
+                  />
+                </div>
+                <div className="hidden md:block">
+                  <div className="ml-10 flex items-baseline space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? 'bg-gray-900 text-white'
+                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-4 flex items-center md:ml-6">
+                  <button
+                    type="button"
+                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                  >
+                    <span className="absolute -inset-1.5" />
+                    <span className="sr-only">View notifications</span>
+                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+
+                  {/* Profile dropdown */}
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span className="absolute -inset-1.5" />
+                        <span className="sr-only">Open user menu</span>
+                        <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
+                      </Menu.Button>
                     </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-        <Droppable droppableId="rows" type="ROW">
-          {(provided) => (
-            <div
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-              className="table"
-            >
-              {data.map((row, rowIndex) => (
-                <Draggable key={row.id} draggableId={`${row.id}_${rowIndex}`} index={rowIndex} type="ROW">
-                  {(provided) => (
-                    <div
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      ref={provided.innerRef}
-                      className="row"
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
                     >
-                      {columns.map((col) => (
-                        <ResizableBox
-                          key={col.id}
-                          width={200}
-                          height={30}
-                          minConstraints={[100, Infinity]}
-                          maxConstraints={[400, Infinity]}
-                          axis="x"
-                          handle={<span className="resize-handle" />}
-                        >
-                          {row[col.name]}
-                        </ResizableBox>
-                      ))}
-                    </div>
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        {userNavigation.map((item) => (
+                          <Menu.Item key={item.name}>
+                            {({ active }) => (
+                              <a
+                                href={item.href}
+                                className={classNames(
+                                  active ? 'bg-gray-100' : '',
+                                  'block px-4 py-2 text-sm text-gray-700'
+                                )}
+                              >
+                                {item.name}
+                              </a>
+                            )}
+                          </Menu.Item>
+                        ))}
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
+              </div>
+              <div className="-mr-2 flex md:hidden">
+                {/* Mobile menu button */}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                   )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
+                </Disclosure.Button>
+              </div>
             </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-    </div>
+          </div>
+
+          <Disclosure.Panel className="md:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+            <div className="border-t border-gray-700 pb-3 pt-4">
+              <div className="flex items-center px-5">
+                <div className="flex-shrink-0">
+                  <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                </div>
+                <div className="ml-3">
+                  <div className="text-base font-medium leading-none text-white">{user.name}</div>
+                  <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
+                </div>
+                <button
+                  type="button"
+                  className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="mt-3 space-y-1 px-2">
+                {userNavigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+
+    <header className="bg-white shadow">
+      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+      </div>
+    </header>
+    <main>
+      <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
+    </main>
+  </div>
   );
 };
 
